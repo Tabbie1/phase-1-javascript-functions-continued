aa73a4a6b1ccdd126f38b351b1241898e9165773
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-15 16:51:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+function saturdayFun(param='roller-skate!'){
+return `This Saturday, I want to ${param}`;
+}
+console.log(saturdayFun());
+
+console.log(saturdayFun('visit the park'));
+
+
+function mondayWork(work='I hate to '){
+
+    return `${work} go to the office`;
+}
+
+console.log(mondayWork());
+console.log(mondayWork('I love to'));
+
+function wrapAdjective(flair='@'){
+    return function(param='special'){
+
+        return `You are ${flair}${param}${flair}`;
+    }
+   
+}
+
+const myfunction = wrapAdjective();
+console.log(myfunction());
+
+console.log(myfunction('a good boy'));
+
+
