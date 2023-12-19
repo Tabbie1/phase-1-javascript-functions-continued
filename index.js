d8274ce573dc21e90c71c2dfcc3783a3d12b7cc4
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-19 19:43:36 +0300

    punctuation and other changes made

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-function saturdayFun(param='roller-skate!'){
-return `This Saturday, I want to ${param}`;
-}
-console.log(saturdayFun());
-
-console.log(saturdayFun('visit the park'));
 
 
-function mondayWork(work='I hate to '){
-
-    return `${work} go to the office`;
+function saturdayFun(param='roller-skate'){
+return `This Saturday, I want to ${param}!`;
 }
 
-console.log(mondayWork());
-console.log(mondayWork('I love to'));
+//console.log(saturdayFun());
+
+//console.log(saturdayFun('visit the park!'));
+
+
+function mondayWork(work='go to the office'){
+
+    return `This Monday, I will ${work}.`;
+}
+
+//console.log(mondayWork());
+//console.log(mondayWork('work from home'));
 
 function wrapAdjective(flair='@'){
     return function(param='special'){
 
-        return `You are ${flair}${param}${flair}`;
+        return `You are ${flair}${param}${flair}!`;
     }
    
 }
